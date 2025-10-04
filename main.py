4806916a667c83f0291ef071a698b89db1e3eb5e
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2025-10-04 01:22:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pdfv-ai-chatbot-hosttest.vercel.app"],  # frontend origin
+    allow_origins=["Front-end-origin"],  # frontend origin note: some fucking dumbass left this out in the open...
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
